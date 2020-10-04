3d1281ea4d7e8b15f60cc0585edebf732abcbb5f
shreyansh2047151 <69526462+shreyansh2047151@users.noreply.github.com>
2020-10-05 01:54:02 +0530

    Update Main_class.java

--- Main_class.java
+++ Main_class.java
@@ -1,7 +1,7 @@
 //1.Implement the concept of control statement and array
 //2.Implement the concept of class,data members,member functions and access specifier.
-//1.Implement the concept of control statement and array
-//2.Implement the concept of class,data members,member functions and access specifier.
+//3.Implement the concept of function overloading and constructor overloading.
+//4.Implement the static keyword- static variable, static block, static function and static class.
 import java.util.*;
 public class Main
 {
